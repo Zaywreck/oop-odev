c829d719c64fe6862d52aba016272a7574dd1c8f
Zaywreck <mgulle35@gmail.com>
2022-10-18 22:30:33 +0300

    new file:   weeks/week 3/CartItem.cs 	new file:   weeks/week 3/Product.cs 	new file:   weeks/week 3/Program.cs 	new file:   weeks/week 3/ShoppingCart.cs 	new file:   weeks/week 3/week 3.csproj 	new file:   weeks/week 3/week 3.sln

---
+++ weeks/week 3/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_3
+{
+    public class Product
+    {
+        public int Price { get; set; }
+
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        public Product(int price, int productID, string productName)
+        {
+            Price = price;
+            ProductID = productID;
+            ProductName = productName;
+        }
+
+        public override string? ToString()
+        {
+            return $"Price: {Price} Product Id: {ProductID} Product Name: {ProductName}";
+        }
+    }
+}
---
+++ weeks/week 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using week_3;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Product Eti = new Product(12, 1, "Biscuit");
+        Product Ulker = new Product(8, 2, "Chocolate");
+        Product Haribo = new Product(15, 3, "Jellybeans");
+
+        CartItem cartItem_1 = new CartItem(1, Eti, 2);
+
+        CartItem cartItem_2 = new CartItem(2, Ulker, 5);
+
+        CartItem cartItem_3 = new CartItem(3, Haribo, 1);
+
+
+        ShoppingCart Cart = new ShoppingCart();
+
+        Cart.Add(cartItem_1);
+        Cart.Add(cartItem_2);
+        Cart.Add(cartItem_3);
+        Console.WriteLine(Cart.GetBalance());
+        Cart.Remove(2);
+        Cart.GetList();
+
+
+    }
+}
